bbb64fa72431d7ddd172f538b2b81907ed9d9823
Fefiria <fyeow007@gmail.com>
2026-07-18 19:08:03 +0700

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'username', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasUuids;
+    protected $primaryKey = 'id_user';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+
+    public function uniqueIds(): array
+    {
+        return ['id_user'];
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
---
+++ app/Services/CloudinaryService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use Cloudinary\Cloudinary;
+
+class CloudinaryService
+{
+    protected Cloudinary $cloudinary;
+
+    public function __construct()
+    {
+        $this->cloudinary = new Cloudinary([
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key'    => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+            ],
+            'url' => [
+                'secure' => true,
+            ],
+        ]);
+    }
+
+    public function upload($file)
+    {
+        return $this->cloudinary
+            ->uploadApi()
+            ->upload($file->getRealPath());
+    }
+
+    public function destroy($publicId)
+    {
+        return $this->cloudinary
+            ->uploadApi()
+            ->destroy($publicId);
+    }
+}
